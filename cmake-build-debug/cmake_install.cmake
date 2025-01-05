36c70db3045d2255bc02678118a0caffcda6922d
Akkuun <mathisduban@gmail.com>
2025-01-05 19:03:15 +0100

    new change in titel

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mathis/Documents/M1_IMAGINE/Prog_3D/Projet_Ray_Tracing
+# Install script for directory: /home/mathis/Documents/M1_IMAGINE/Prog_3D/rayTracingFolderGithub
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -57,6 +57,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/home/mathis/Documents/M1_IMAGINE/Prog_3D/Projet_Ray_Tracing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/mathis/Documents/M1_IMAGINE/Prog_3D/rayTracingFolderGithub/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
